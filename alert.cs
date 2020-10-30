617fbcf53b1aa43174796adcf8270fb627575727
Jason Hunt <huntjason@live.com>
2020-10-30 11:57:27 -0400

    change to csharp add workspace

---
+++ alert.cs
@@ -0,0 +1,18 @@
+using System;
+
+
+namespace Alert
+{
+    public class Watcher{
+        public Watcher(){
+            
+        }
+    }
+    class Alert{
+        static void Main()
+        {
+        Console.WriteLine("Press any key to exit.");
+        Console.ReadKey();
+        }
+    }
+}
