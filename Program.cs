a9c2bec5390375473696e899cfe58cb1273145f1
Jason Hunt <huntjason@live.com>
2020-10-30 13:35:58 -0400

    Work on watcher to parse config

--- Program.cs
+++ Program.cs
@@ -1,14 +1,67 @@
 ﻿using System;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using CommandLine;
+using System.Collections.Generic;
+using System.IO;
 
 namespace OrderBot
 {
+    //commandline parsing
+    class Options
+    {
+        [Option('c', "config", Required = true, HelpText = "Path to config file")]
+        public string config { get; set; }
+
+    }
     class Program
     {
+        private static List<Watcher> watchers {get; set;}
+
         static void Main(string[] args)
         {
+            Options ops = new Options();
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(parsed => ops = parsed);
+            watcher_initalize(ops.config);
             Console.WriteLine("Hello World!");
+        }
+
+        private static void watcher_initalize(string config_path){
+            watchers = new List<Watcher>();
+            //open config
+            using(StreamReader reader = new StreamReader(config_path)){
+                string line;
+                string name = "";
+                Watcher watcher = null;
+                //read line by line
+                while((line = reader.ReadLine()) != null){
+                    //skip over comments
+                    if(line.StartsWith("###"))
+                        continue;
+                    //create new watcher with this name
+                    if(line.StartsWith("name:")){
+                        //create new watcher
+                        name = line.Substring(5);
+                        Console.WriteLine($"Creating new {name}: Watcher...");
+                        watcher = new Watcher(name);
+                        //add watcher to list
+                        watchers.Add(watcher);
+                    }
+                    else if(line.StartsWith("url:")){
+                        var pair = line.Substring(4).Split(':');
+                        //add url
+                        Console.WriteLine($"adding url {pair[0]} to watcher {watcher.watcherName}");
+                        watcher.urls.Add(pair[0].Trim());
+                        
+                        //add url xpath pair
+                        Console.WriteLine($"adding xpath {pair[1]} to watcher {watcher.watcherName}");
+                        watcher.urlXPath.Add(pair[0].Trim(), pair[1].Trim());
+
+                    }
+
+                }
+            }
         }
     }
 }
--- alert.cs
+++ alert.cs
@@ -1,11 +1,55 @@
 using System;
-
+using System.Collections.Generic;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace OrderBot
 {
     public class Watcher{
-        public Watcher(){
-            
+        //Fields
+        #region fields
+        public string watcherName {get; set;}
+        public List<String> phoneNumbers {get; set;}
+        public List<String> urls {get; set;}
+        //List of browsers for each given url from config
+        public List<IWebDriver> webDrivers {get; set;}
+//url is key xpath is value
+        public IDictionary<string, string> urlXPath {get; set;}
+        #endregion
+        //constructor takes watcher name and path to config
+        public Watcher(string watcherName){
+            //set watcher name
+            this.watcherName = watcherName;
+            //instantiate lists
+            this.phoneNumbers = new List<String>();
+            this.urls = new List<String>();
+            this.webDrivers = new List<IWebDriver>();
+            //instantiate map
+            urlXPath =  new Dictionary<string, string>();
+        }
+        private void createWebDrivers(){
+            foreach(var url in urls){
+                var driver = new ChromeDriver();
+                driver.Url = url;
+                //add driver to list
+                webDrivers.Add(driver);
+            }
+        }
+        public void refresh(){
+            //go through driver and refresh 
+            string xpath = "";
+
+            foreach(var driver in webDrivers){
+                driver.Navigate().Refresh();
+                if(urlXPath.TryGetValue(driver.Url, out xpath)){
+                    var element = driver.FindElement(By.XPath(xpath));
+                    Console.WriteLine(element.Text);
+                }
+                else{
+                    Console.WriteLine($"XPath for Url:{driver.Url} not in map check config file!");
+                }
+            }
         }
     }
+
 }
