a8909eae7b3b2480b0c55c8743d7846d5e0ffd08
Jason Hunt <huntjason@live.com>
2020-10-29 23:57:10 -0400

    change arg name chane config file name

--- alert.py
+++ alert.py
@@ -9,11 +9,13 @@ logging.basicConfig(filename="AlertScript.log",
 logger = logging.getLogger
 ##argument parser
 parser = argparse.ArgumentParser(description='Take Config File Path')
-parser.add_argument('config',action='store', type=str, help='Filepath to config file')
+parser.add_argument('--config', required=True, help='Filepath to config file')
 args = parser.parse_args()
 
 def initalize():
     print("Initalize")
+
+
 def main():
     initalize()
 
